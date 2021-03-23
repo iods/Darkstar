514813e6fac7adaba5330c173dd65f3a5b18925a
Rye Miller <millerrye17@gmail.com>
2021-03-23 00:02:55 -0600

    Addition of mining strategy interface for PoW.

--- examples/ravino/src/Miner/ProofOfWork.php
+++ examples/ravino/src/Miner/ProofOfWork.php
@@ -7,3 +7,49 @@
  *
  * @filesource
  */
+namespace Darkstar\Ravino\Miner;
+
+use Darkstar\Ravino\Block\BlockInterface;
+
+
+/**
+ * Class ProofOfWork
+ * @package Darkstar\Ravino\Miner
+ */
+final class ProofOfWork implements MinerInterface
+{
+    private int $difficulty;
+
+    /**
+     * ProofOfWork constructor.
+     * @param int $difficulty
+     */
+    public function __construct(int $difficulty)
+    {
+        $this->difficulty = $difficulty;
+    }
+
+    /**
+     * @param BlockInterface $block
+     * @return bool
+     */
+    public function mine(BlockInterface $block): bool
+    {
+        $prefix = str_repeat('0', $this->difficulty);
+
+        while(substr($block->getHash(), 0, $this->difficulty) != $prefix) {
+            $block->setNonce($block->getNonce() + 1);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param BlockInterface $block
+     * @return bool
+     */
+    public function validate(BlockInterface $block): bool
+    {
+        return $block instanceof ProofOfWorkBlockInterface;
+    }
+}
