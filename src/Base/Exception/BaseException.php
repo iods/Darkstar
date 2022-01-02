03792060429734bbfa07dd513cfdc31a883c170b
Rye Miller <rmiller@corra.com>
2022-01-02 14:26:34 -0700

    Addition of base exception in container scope.

---
+++ src/Base/Exception/BaseException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Darkstar PHP SDK.
+ *
+ * @version v000.1.0 Initial Release
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ *
+ * @filesource
+ */
+declare(strict_types=1);
+
+namespace Iods\Base\Exception;
+
+use Darkstar\Framework\Exception\{AbstractException, ExceptionInterface};
+
+class BaseException extends AbstractException implements ExceptionInterface
+{
+    // ...
+}
