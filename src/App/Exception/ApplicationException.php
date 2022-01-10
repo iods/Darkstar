f202721bd0f69a865f97b2e71d67d1957e98ef5e
Rye Miller <rmiller@corra.com>
2022-01-09 19:08:40 -0700

    Addition of PHPUnit testing to the DI Service.

---
+++ src/App/Exception/ApplicationException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Iods PHP SDK.
+ *
+ * @version 000.1.0 Initial Release
+ * @license The MIT License, http://opensource.org/licenses/MIT
+ *
+ * @filesource
+ */
+declare(strict_types=1);
+
+namespace Iods\App\Exception;
+
+use Iods\Base\Exception\BaseException;
+
+class ApplicationException extends BaseException
+{
+    // ...
+}
